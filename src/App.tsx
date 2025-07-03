09c64b9858c0945d8baa2f9d677b36c72e0894e7
Kate Dowsley <kate.r.dowsley@gmail.com>
2025-07-03 20:43:57 +1000

    Start photo carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
-import { useState } from 'react'
-import testShirt from '/test_shirt.jpg'
-import './App.css'
+import { useState } from 'react';
+import testShirt from '/test_shirt.jpg';
+import testShirt2 from '/test_shirt2.jpg';
+import testShirt3 from '/test_shirt3.jpg';
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [imageIndex, setImageIndex] = useState(0);
+  let imageList: string[] = [testShirt, testShirt2, testShirt3];
 
   return (
     <>
       <div>
-        <img src={testShirt} className="clothes-im" alt="test image"/>
+        <img src={testShirt} className="clothes-im rotate" alt="test image" />
       </div>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setImageIndex((count) => {
+          if (count - 1 > 0) { return count - 1; }
+          else { return imageList.length-1; }
+        })}>
+          &lt;
+        </button>
+        <img src={imageList[imageIndex]} className="clothes-im" alt = "selected shirt"/>
+        <button onClick={() => setImageIndex(count => {
+          if (count+1<imageList.length){return count + 1;}
+          else{return 0}
+        })}>
+          <div className='arrow-right'></div>
         </button>
         <p>
-          <code>src/App.tsx</code> and save to test HMR
+          <code>src/App.tsx</code> and save to test HMRdfg
         </p>
       </div>
     </>
