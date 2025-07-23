f2cf04c320b01ca6ca3fa5ec984fea6b7c61562d
Kate Dowsley <kate.r.dowsley@gmail.com>
2025-07-23 21:06:02 +1000

    Create photo carousel

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,41 @@ import testShirt3 from '/test_shirt3.jpg';
 import './App.css';
 
 function App() {
-  const [imageIndex, setImageIndex] = useState(0);
+
   let imageList: string[] = [testShirt, testShirt2, testShirt3];
+
+  interface idxData {
+    prev: number,
+    curr: number,
+    next: number
+  }
+  const [imageIndex, setImageIndex] = useState<idxData>({
+    prev: imageList.length - 1,
+    curr: 0,
+    next: 1
+  });
+
+  const updateIdx = function (direction: string, currIdx: number, maxIdx: number): number {
+    if (direction == "+") {
+      if (currIdx + 1 < maxIdx) { return currIdx + 1; }
+      else { return 0; }
+    }
+    else if (direction == "-") {
+      if (currIdx - 1 >= 0) { return currIdx - 1; }
+      else { return maxIdx - 1; }
+    }
+    else return 0;
+  }
+
+  const updateImIdx = (direction: string) => {
+    setImageIndex(imageIndex => ({
+      prev: updateIdx(direction, imageIndex.prev, imageList.length),
+      curr: updateIdx(direction, imageIndex.curr, imageList.length),
+      next: updateIdx(direction, imageIndex.next, imageList.length)
+    }));
+  };
+
+  
 
   return (
     <>
@@ -15,17 +48,14 @@ function App() {
         <img src={testShirt} className="clothes-im rotate" alt="test image" />
       </div>
       <div className="card">
-        <button onClick={() => setImageIndex((count) => {
-          if (count - 1 > 0) { return count - 1; }
-          else { return imageList.length-1; }
-        })}>
+        <button onClick={() => updateImIdx("-")}>
           &lt;
         </button>
-        <img src={imageList[imageIndex]} className="clothes-im" alt = "selected shirt"/>
-        <button onClick={() => setImageIndex(count => {
-          if (count+1<imageList.length){return count + 1;}
-          else{return 0}
-        })}>
+        <img src={imageList[imageIndex.prev]} className="clothes-im" alt="selected shirt" />
+        <img src={imageList[imageIndex.curr]} className="clothes-im" alt="selected shirt" />
+        <img src={imageList[imageIndex.next]} className="clothes-im" alt="selected shirt" />
+
+        <button onClick={() => updateImIdx("+")}>
           <div className='arrow-right'></div>
         </button>
         <p>
